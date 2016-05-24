936fa197f92582924424368203329941a35688e0
Maxime Forget-Séguin <maxime.forget-seguin@radio-canada.ca>
2016-05-24 13:23:06 -0400

    update to koco2

--- src/select2-binding-handler.js
+++ src/select2-binding-handler.js
@@ -1,69 +1,65 @@
-define([
-        'knockout',
-        'jquery',
-        'disposer'
-    ],
-    function(ko, $, KoDisposer) {
-        'use strict';
+import ko from 'knockout';
+import $ from 'jquery';
+import KoDisposer from 'disposer';
 
-        //TODO: Faire en sorte que ce binding applique implicitement les bindings de "options"
-        ko.bindingHandlers.select2 = {
-            init: function(element, valueAccessor, allBindingsAccessor) {
-                var obj = valueAccessor(),
-                    allBindings = allBindingsAccessor(),
-                    lookupKey = allBindings.lookupKey,
-                    select2Options = allBindings.select2Options,
-                    select2SelectedOption = allBindings.select2SelectedOption,
-                    options = allBindings.options,
-                    value = allBindings.value,
-                    $element = $(element);
 
-                $element.select2(obj);
+//TODO: Faire en sorte que ce binding applique implicitement les bindings de "options"
+ko.bindingHandlers.select2 = {
+    init: function(element, valueAccessor, allBindingsAccessor) {
+        var obj = valueAccessor(),
+            allBindings = allBindingsAccessor(),
+            lookupKey = allBindings.lookupKey,
+            select2Options = allBindings.select2Options,
+            select2SelectedOption = allBindings.select2SelectedOption,
+            options = allBindings.options,
+            value = allBindings.value,
+            $element = $(element);
 
-                var koDisposer = new KoDisposer();
+        $element.select2(obj);
 
-                ko.utils.domNodeDisposal.addDisposeCallback(element, function() {
-                    koDisposer.dispose();
-                });
+        var koDisposer = new KoDisposer();
 
-                if (options && ko.isObservable(options)) {
-                    koDisposer.add(options.subscribe(function() {
-                        $element.removeClass('select2-offscreen').select2(obj);
-                    }));
-                }
+        ko.utils.domNodeDisposal.addDisposeCallback(element, function() {
+            koDisposer.dispose();
+        });
 
-                if (value && ko.isObservable(value)) {
-                    koDisposer.add(value.subscribe(function() {
-                        $element.removeClass('select2-offscreen').select2(obj);
-                    }));
-                }
+        if (options && ko.isObservable(options)) {
+            koDisposer.add(options.subscribe(function() {
+                $element.removeClass('select2-offscreen').select2(obj);
+            }));
+        }
 
-                if (select2Options && ko.isObservable(select2Options)) {
-                    koDisposer.add(select2Options.subscribe(function() {
-                        $element.removeClass('select2-offscreen').select2(obj);
-                    }));
-                }
+        if (value && ko.isObservable(value)) {
+            koDisposer.add(value.subscribe(function() {
+                $element.removeClass('select2-offscreen').select2(obj);
+            }));
+        }
 
-                if (select2SelectedOption && ko.isObservable(select2SelectedOption)) {
-                    koDisposer.add(select2SelectedOption.subscribe(function() {
-                        $element.trigger('change');
-                    }));
-                }
+        if (select2Options && ko.isObservable(select2Options)) {
+            koDisposer.add(select2Options.subscribe(function() {
+                $element.removeClass('select2-offscreen').select2(obj);
+            }));
+        }
 
-                if (lookupKey) {
-                    var x = ko.unwrap(allBindings.value);
+        if (select2SelectedOption && ko.isObservable(select2SelectedOption)) {
+            koDisposer.add(select2SelectedOption.subscribe(function() {
+                $element.trigger('change');
+            }));
+        }
 
-                    $element.select2('data', ko.utils.arrayFirst(obj.data.results, function(item) {
-                        return item[lookupKey] === x;
-                    }));
-                }
+        if (lookupKey) {
+            var x = ko.unwrap(allBindings.value);
 
-                ko.utils.domNodeDisposal.addDisposeCallback(element, function() {
-                    $element.select2('destroy');
-                });
-            },
-            update: function(element) {
-                $(element).trigger('change');
-            }
-        };
-    });
+            $element.select2('data', ko.utils.arrayFirst(obj.data.results, function(item) {
+                return item[lookupKey] === x;
+            }));
+        }
+
+        ko.utils.domNodeDisposal.addDisposeCallback(element, function() {
+            $element.select2('destroy');
+        });
+    },
+    update: function(element) {
+        $(element).trigger('change');
+    }
+};
